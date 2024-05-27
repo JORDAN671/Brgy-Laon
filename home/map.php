2a19eac8fc258d5cc19f8b3f1496ce1485d10dbe
JORDAN671 <160459143+JORDAN671@users.noreply.github.com>
2024-05-27 14:34:20 +0800

    Add files via upload

---
+++ home/map.php
@@ -0,0 +1,8 @@
+<div class="container text-light">
+    <div class="text-center mt-5">
+        <h1 class="welcome_text">MAP OF THE BARANGAY</h1>
+    </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15519.11207961362!2d121.84610352506407!3d13.487771606081592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a3279d411f5d4b%3A0x300713c3869d14dc!2sLaon%2C%20Mogpog%2C%20Marinduque!5e0!3m2!1sen!2sph!4v1716552308768!5m2!1sen!2sph" width="1100" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+
